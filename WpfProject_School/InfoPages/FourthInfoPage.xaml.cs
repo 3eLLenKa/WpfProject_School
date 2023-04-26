1322609b8d24c116237e49ad6ba22a2d2bef718c
Дмитрий Бейкун <alexkomkov39@gmail.com>
2023-04-26 03:54:05 +0200

    commit all

---
+++ WpfProject_School/InfoPages/FourthInfoPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfProject_School.InfoPages
+{
+    /// <summary>
+    /// Логика взаимодействия для FourthInfoPage.xaml
+    /// </summary>
+    public partial class FourthInfoPage : Page
+    {
+        public FourthInfoPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GoBack(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
--- WpfProject_School/MVVM/View/GeneralView.xaml.cs
+++ WpfProject_School/MVVM/View/GeneralView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfProject_School.InfoPages;
 
 namespace WpfProject_School.MVVM.View
 {
@@ -28,6 +29,26 @@ namespace WpfProject_School.MVVM.View
         private void LevelsPageClick(object sender, RoutedEventArgs e)
         {
             NavigationService.GetNavigationService(this).Navigate(new LevelsPage());
+        }
+
+        private void GoToFirstInfoPage(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GetNavigationService(this).Navigate(new FirstInfoPage());
+        }
+
+        private void GoToSecondInfoPage(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GetNavigationService(this).Navigate(new SecondInfoPages());
+        }
+
+        private void GoToThirdInfoPage(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GetNavigationService(this).Navigate(new ThirdInfoPage());
+        }
+
+        private void GoToFourthInfoPage(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GetNavigationService(this).Navigate(new FourthInfoPage());
         }
     }
 }
